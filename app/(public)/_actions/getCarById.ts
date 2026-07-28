c5ac2fb9f8069aca3195c394edbef79cfc2a667a
Alamin4D <alaminahmed79000@gmail.com>
2026-07-28 12:57:42 +0600

    feat: implement get car by id API integration

--- app/(public)/_actions/getCarById.ts
+++ app/(public)/_actions/getCarById.ts
@@ -2,6 +2,7 @@ export const getCarById = async (id: string) => {
     const res = await fetch(`http://localhost:5000/api/v1/cars/${id}`, {
         cache: "no-store",
     });
+    
 
     if (!res.ok) {
         throw new Error("Failed to fetch car");
